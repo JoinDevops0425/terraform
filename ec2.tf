b642c50f53ca9c87f1edc9e69a02b6849c619ca7
JoinDevops0425 <joindevops0425@gmail.com>
2025-06-19 19:41:42 +0530

    adding rest of the files

---
+++ ec2.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_all" {
+    name = "allow all 2"
+    description = "allow all traffic"
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow-all-from-tags"
+    }
+
+
+}
